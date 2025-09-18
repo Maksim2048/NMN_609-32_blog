5d4aa50e193e92df0e8739fc12b003fd70824343
Maksim Nikulin <nikulin-max04-05@yandex.ru>
2025-09-18 11:06:50 +0300

    -update

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import CreateArticle
 
@@ -23,4 +24,33 @@ def article_create(request):
             return redirect('homepage')
     else:
         form = CreateArticle()
-    return render(request, 'articles/article_create.html', {'form': form})
+    return render(request, 'articles/article_form.html', {'form': form})
+
+@login_required(login_url='accounts:login')
+def article_update(request, slug):
+    article = Article.objects.get(slug=slug)
+    if request.user.id == article.author.id:
+        if request.method == 'POST':
+            form = CreateArticle(request.POST, request.FILES, instance=article)
+            if form.is_valid():
+                instance = form.save(commit=False)
+                instance.author = request.user
+                instance.save()
+                return redirect(to='articles:article_detail', slug=article.slug)
+        else:
+            form = CreateArticle(instance=article)
+        return render(request, 'articles/article_form.html', {'form': form})
+    return HttpResponse('401 Unauthorized', status=401)
+
+@login_required(login_url='accounts:login')
+def article_delete(request, slug):
+    article = Article.objects.get(slug=slug)
+    if request.user.id == article.author.id:
+        if request.method == 'POST':
+            article.delete()
+            return redirect('homepage')
+        return render(request, 'articles/article_confirm_delete.html', {'article': article})
+    return HttpResponse('401 Unauthorized', status=401)
+
+
+
