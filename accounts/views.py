590c1d3d8b8a57bb1d6f6cc99fbc020f7dd1fed5
Maksim Nikulin <nikulin-max04-05@yandex.ru>
2025-09-22 20:45:25 +0300

    -update

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,23 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
+
+from mysite.accounts.forms import SignupForm, LoginForm
+
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('/articles')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = LoginForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
@@ -23,7 +25,7 @@ def login_view(request):
                 return redirect(request.POST['next'])
             return redirect('homepage')
     else:
-        form = AuthenticationForm()
+        form = LoginForm()
     return render(request, 'accounts/login.html', {'form': form})
 
 def logout_view(request):
